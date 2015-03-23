60cdc8acae472ef966470175dfff31aed9fb9f38
sijayaraman <sijayaraman@paypal.com>
2015-03-23 20:35:22 +0530

    Fixed the cancel button issue

--- Spring-RestUsingMySQLAndAngularJs/target/classes/public/js/app.js
+++ Spring-RestUsingMySQLAndAngularJs/target/classes/public/js/app.js
@@ -2,11 +2,11 @@
 var app = angular.module("app", [ "xeditable" ])
 app.controller('EditableRowCtrl', function($scope, $filter, $http, $q) {
 
-	//$scope.isNewCreatedRow = false;
-	//Get the employeeList
+	// $scope.isNewCreatedRow = false;
+	// Get the employeeList
 	getEmployeeList();
 
-	//Generate the list from db
+	// Generate the list from db
 	function getEmployeeList()
 	{
 		$http.get('http://localhost:9999/employee/list').success(
@@ -15,7 +15,7 @@ app.controller('EditableRowCtrl', function($scope, $filter, $http, $q) {
 				});
 	}
 
-	//Save user
+	// Save user
 	$scope.saveUser = function(data, id, age, name, location) {
 
 		angular.extend(data, {
@@ -34,18 +34,12 @@ app.controller('EditableRowCtrl', function($scope, $filter, $http, $q) {
 		}
 		else
 		{
-			//Modifying the existing row
+			// Modifying the existing row
 			return $http.put('http://localhost:9999/employee/modify', data);
 		}
 
 
 	};
-
-	//Cancel user
-	$scope.cancel = function() {
-		console.log("test"+$scope.isNewCreatedRow);
-		getEmployeeList();
-	}
 
 	  
 	// add user
@@ -65,16 +59,59 @@ app.controller('EditableRowCtrl', function($scope, $filter, $http, $q) {
 
 
 	// remove user
-	$scope.removeUser = function(index) {
-		var emp = $scope.employees[index];
-		console.log(emp);
-		$http.delete('http://localhost:9999/employee/delete/'+emp.id).success(function (data) {
-			//reload the list after delete
+	$scope.removeUser = function(data,id) {
+		console.log("ID",id);
+		$http.delete('http://localhost:9999/employee/delete/'+id).success(function (data) {
+			// reload the list after delete
 			getEmployeeList();
 
 		})
 	};
 
+	$scope.cancelAdvice = function(rowform, index){
+		if($scope.isNewCreatedRow){
+			console.log(rowform, index);
+			$scope.employees.splice(index,1);
+			rowform.$cancel();
+		}
+		    getEmployeeList();
+		}
+	
+	
+	// Validate Id
+	 $scope.checkId = function(data,id) {
+		 console.log("Id=",data);
+		    if(data.length == 0){
+		    	return "ID should not be Empty";
+		    }
+			if(data == 0){
+				return "ID should not be zero";
+			}
+			
+			if(data < 0){
+				return "ID should not be Negative";
+			}
+			
+			if(isNaN(data)){
+				return  "ID should be in Number";
+			}
+			
+			
+			
+		  };
+
+		  // Validate Age
+		  $scope.checkAge = function(data,age) {
+			  console.log("Age=",data);
+			    if(data.length == 0){
+			    	return "Age should not be Empty";
+			    }
+			    
+			  if (isNaN(data)) {
+				  return "Age should be in number";
+			  }
+		  };
+				  
 	$scope.sortField = 'id';
 	$scope.reverse = true;
 
